31dfa10c3e51bc56005bc6e920ca89ffaeaf198b
André Freitas <freitasandre38@gmail.com>
2023-07-19 17:17:00 -0300

    feat: adiciona rotas básicas usando fast apit

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, APIRouter
+
+
+app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
+
+api_router = APIRouter()
+
+
+@api_router.get("/", status_code=200)
+def root() -> dict:
+    """
+    Root GET
+    """
+    return {"msg": "Hello, World!"}
+
+@api_router.get("/content", status_code=404)
+def root() -> dict:
+    return {"CONTENT:" : "You are trying to access invalid content"}
+
+
+app.include_router(api_router)
+
+
+if __name__ == "__main__":
+    # Use this for debugging purposes only
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
