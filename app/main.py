f72a487403b84b19f38bd8547edb2514ea58c27a
André Freitas <freitasandre38@gmail.com>
2023-08-14 14:13:57 -0300

    feat: creating new query route

--- app/main.py
+++ app/main.py
@@ -1,48 +1,30 @@
-from fastapi import FastAPI, HTTPException, Depends, Request, APIRouter
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import Session
-from models import User
-from database import SessionLocal, engine
-from pydantic import BaseModel
+from fastapi import FastAPI, APIRouter
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+from typing import Optional
 
-class UserResponse(BaseModel):
-    id: int
-    username: str
-    email: str
-
-app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
-
-api_router = APIRouter()
 
 RECIPES = [
     {
         "id": 1,
         "label": "Chicken Vesuvio",
         "source": "Serious Eats",
-        "url": "http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html",
     },
     {
         "id": 2,
         "label": "Chicken Paprikash",
         "source": "No Recipes",
-        "url": "http://norecipes.com/recipe/chicken-paprikash/",
     },
     {
         "id": 3,
         "label": "Cauliflower and Tofu Curry Recipe",
         "source": "Serious Eats",
-        "url": "http://www.seriouseats.com/recipes/2011/02/cauliflower-and-tofu-curry-recipe.html",
     },
 ]
+
+
+app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
+
+api_router = APIRouter()
 
 
 @api_router.get("/", status_code=200)
@@ -50,11 +32,7 @@ def root() -> dict:
     """
     Root GET
     """
-    return {"msg": "Hello, World!"}
-
-@api_router.get("/content", status_code=404)
-def root() -> dict:
-    return {"CONTENT:" : "You are trying to access invalid content"}
+    return {"msg": "Tasting Fast-API !!!"}
 
 
 @api_router.get("/recipe/{recipe_id}", status_code=200)
@@ -67,17 +45,28 @@ def fetch_recipe(*, recipe_id: int) -> dict:
     if result:
         return result[0]
 
-@api_router.get("/users/", response_model=list[UserResponse])
-def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    users = db.query(User).offset(skip).limit(limit).all()
-    return users
+
+# https://fastapi.tiangolo.com/tutorial/query-params/
+@api_router.get("/search/", status_code=200)
+def search_recipes(
+    keyword: Optional[str] = None, max_results: Optional[int] = 10
+) -> dict:
+    """
+    Search for recipes based on label keyword
+    """
+    if not keyword:
+        # RECIPES está sendo limitado por max_results
+        return {"results": RECIPES[:max_results]}
+
+        # Estudar mais sobre expressões lambda em python
+    results = filter(lambda recipe: keyword.lower() in recipe["label"].lower(), RECIPES)
+    return {"results": list(results)[:max_results]}
 
 
 app.include_router(api_router)
 
 
 if __name__ == "__main__":
-    # Use this for debugging purposes only
     import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
