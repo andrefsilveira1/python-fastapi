245ee714443ffdaceb526b403ce56a08c80eaa90
André Freitas <freitasandre38@gmail.com>
2023-08-14 14:20:17 -0300

    refactor: remove recipes from main and put in the schema

--- app/main.py
+++ app/main.py
@@ -3,25 +3,6 @@ from fastapi import FastAPI, APIRouter
 from typing import Optional
 
 
-RECIPES = [
-    {
-        "id": 1,
-        "label": "Chicken Vesuvio",
-        "source": "Serious Eats",
-    },
-    {
-        "id": 2,
-        "label": "Chicken Paprikash",
-        "source": "No Recipes",
-    },
-    {
-        "id": 3,
-        "label": "Cauliflower and Tofu Curry Recipe",
-        "source": "Serious Eats",
-    },
-]
-
-
 app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
 
 api_router = APIRouter()
---
+++ app/recipes.py
@@ -0,0 +1,18 @@
+
+RECIPES = [
+    {
+        "id": 1,
+        "label": "Chicken Vesuvio",
+        "source": "Serious Eats",
+    },
+    {
+        "id": 2,
+        "label": "Chicken Paprikash",
+        "source": "No Recipes",
+    },
+    {
+        "id": 3,
+        "label": "Cauliflower and Tofu Curry Recipe",
+        "source": "Serious Eats",
+    },
+]
